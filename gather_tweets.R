74181b0c5fcf164a9066df1e6b766c35293d278e
jd282 <jd282@duke.edu>
2016-06-30 13:28:08 -0400

    Add files via upload

---
+++ gather_tweets.R
@@ -0,0 +1,51 @@
+library(twitteR)
+library(xlsx)
+
+### Takes a hashtag or search term and outputs the search results on Twitter into an csv file 
+
+#twitter authorization
+api_key = "ahlcqH8WtwV2DtEcZiEvvrHfX"
+api_secret = "nk7dP1L1PuenXmIK0YjY2tFoBu5MdHus2xypr9uefJ8ZBFjbI3"
+access_token = "746420255426129924-16KXlZgdbQqRTs0snJZko3ckBwGtnSv"
+access_token_secret = "cZOlWLvvwfjUgQiOdKbvhO6qkwflAkdMDS1tezo2XmxHn"
+setup_twitter_oauth(api_key,api_secret,access_token,access_token_secret)
+
+#gather search results for hashtag/term
+query = readline(prompt="Enter Twitter query: ")
+tweets = searchTwitter(query, n=200, lang='en') 
+Tweets.text = lapply(tweets,function(t)t$getText())
+Tweets.text = sapply(Tweets.text, function(row) iconv(row, "latin1", "ASCII", sub="")) #gets rid of emojis (temp)
+
+clean_text = function(some_txt){
+    some_txt = gsub("(RT|via)((?:\\b\\W*@\\w+)+)", "", some_txt)
+    some_txt = gsub("@\\w+", "", some_txt)
+    some_txt = gsub("[[:punct:]]", "", some_txt)
+    sentence = gsub('[[:cntrl:]]', '', some_txt)
+    #some_txt = gsub("[[:digit:]]", "", some_txt)
+    some_txt = gsub("http\\w+", "", some_txt)
+    some_txt = gsub("[\t]{2,}", "", some_txt)
+    some_txt = gsub("^\\s+|\\s+$", "", some_txt)
+    
+    # define "tolower error handling" function
+    try.tolower = function(x)
+    {
+      y = NA
+      try_error = tryCatch(tolower(x), error=function(e) e)
+      if (!inherits(try_error, "error"))
+        y = tolower(x)
+      return(y)
+    }
+    
+    some_txt = sapply(some_txt, try.tolower)
+    some_txt = some_txt[some_txt != ""]
+    names(some_txt) = NULL
+    return(some_txt)
+}
+
+#create table with each row as a tweet 
+df_tweets = data.frame(
+  Tweet = sapply(Tweets.text, clean_text)
+)
+
+#save tweets into excel file 
+write.xlsx(df_tweets, "/Users/jenniferdu/Documents/Duke/DataPlus2016/SentimentAnalysis/tweets.xlsx")
---
+++ sentiment_analysis_indicoio.py
@@ -0,0 +1,35 @@
+import csv
+import indicoio
+
+indicoio.config.api_key = "c055aa319b32e209e7862c21f9216377"
+
+data = []
+
+
+#iterate through rows of csv file and calculate the sentiment score of each cell
+csvf = input("Enter a csv file to parse: ")
+with open(csvf, 'rb') as csvfile:
+    rd = csv.reader(csvfile)
+    rownum = 0
+    for row in rd:
+        if rownum != 0:
+            colnum = 0
+            for col in row:
+                if (colnum != 0) and (col != ""):
+                    score = indicoio.sentiment(col)
+                    data.append([rownum, col, score])
+                colnum += 1
+        rownum += 1          
+csvfile.close()
+
+
+#write the score of the tweet into the same csv file
+csvfile = open(csvf, 'wb')
+wr = csv.writer(csvfile)
+wr.writerow(["Tweet", "Sentiment Score"])
+for item in data:
+    wr.writerow(item)
+
+
+
+
