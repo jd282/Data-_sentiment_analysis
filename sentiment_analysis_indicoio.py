4928eb2bd24e7cca2637df3264c1bd7b0c1b4924
jd282 <jd282@duke.edu>
2016-07-13 10:42:43 -0400

    Add files via upload

--- sentiment_analysis_indicoio.py
+++ sentiment_analysis_indicoio.py
@@ -1,40 +1,71 @@
 import csv
 import indicoio
+from operator import truediv
 
-#given csv file of tweets will calculate the sentiment score (from indico) for each
+#given csv file of tweets will iterate through row and calculate the sentiment score
+#(from indico) for each and export to new csv file
+
 #can create a free indico account that will give me 10,0000 free requests 
-indicoio.config.api_key = "c055aa319b32e209e7862c21f9216377"
-
-data = []
+#indicoio.config.api_key = "c055aa319b32e209e7862c21f9216377"    
 
 #iterate through rows of csv file and calculate the sentiment score of each cell
-csvf = input("Enter a csv file to parse: ")
-sent_sum = 0
+#csvf = input("Enter a csv file to parse: ")
+csvf = "tweets.csv"
+
+#information to write into the new csv file 
+data = []
+
+#information to gather from input csv file including column number that tweets,
+#retweets and query are in 
+header = []
+t_idx = None 
+q_idx = None
+queries = set()
+
 with open(csvf, 'rb') as csvfile:
     rd = csv.reader(csvfile)
     rownum = 0
+    tweets = []
+    
     for row in rd:
-        if rownum != 0:
-            colnum = 0
-            for col in row:
-                if (colnum != 0) and (col != ""):
-                    score = indicoio.sentiment(col)
-                    sent_sum += score
-                    data.append([rownum, col, score])
-                colnum += 1
-        rownum += 1          
+        if rownum == 0:
+            #get header information
+            
+            header = row
+            t_idx = header.index("Tweet")
+            q_idx = header.index("Query")
+            
+        else: 
+            q = row[q_idx]
+            t = row[t_idx]
+            if (q not in queries) and (len(queries) != 0):
+                #reset when new query reached
+                tweets = []
+
+            #do not include repeats                 
+            if t in tweets:
+                continue
+            
+            tweets.append(t)
+            queries.add(q)
+            #calculate the score using indicoio API, 
+            score = 1
+            #score = indicoio.sentiment(t)
+            newrow = row
+            newrow.append(score)
+            data.append(newrow)
+
+        rownum += 1
+        
 csvfile.close()
 
-mean = sent_sum/rownum
-
-
-#write the score of the tweet into the same csv file
+#write original info and sentiment score into new csv file 
+csvf = 'tweets_score.csv'
 csvfile = open(csvf, 'wb')
 wr = csv.writer(csvfile)
-wr.writerow(["Tweet", "Sentiment Score"])
+header.append("Sentiment Score")
+wr.writerow(header)
 for item in data:
     wr.writerow(item)
 
 csvfile.close()
-
-
